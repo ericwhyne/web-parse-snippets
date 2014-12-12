28d790caa992cce32e2e5e4bdee218d783aa131f
Eric Whyne <ericwhyne@gmail.com>
2014-12-12 08:27:07 -0500

    json

--- obsess/wiki-munge.py
+++ obsess/wiki-munge.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+# coding=utf-8
 import mwclient
 import json
 import re
 
-mediawiki_account_config = '/home/eric/.ssh/ebola-dev-robot.json'
+mediawiki_account_config = '/home/eric/.ssh/ebola-robot.json'
 
 mwaccount = json.load(open(mediawiki_account_config))
 
@@ -15,14 +16,12 @@ for page in mwsite.pages:
     oldpagetext = page.text()
     fileprefix =  re.sub('[ /]','_',page.page_title)
     if '[[category:organizations]]' in oldpagetext or '[[category:locations]]' in oldpagetext or '[[category:people]]' in oldpagetext:
-      print "Editing ", fileprefix
+#      print "Editing ", fileprefix
       # * [http://reddit.com/r/ebola/comments/2n1jyn/un_news_ebola_ban_hopeful_of_outbreaks_end_by/ Discus on Reddit]
       # * [http://reddit.com/r/EbolaGooglers/comments/2ji2fj/ebola_transmission_can_occur_from_sleeping_on_the/ Discus on Reddit]
 
-      if "http://reddit.com/r/" in oldpagetext:
-        print "found munge"
-        oldpagetext = re.sub('\*.*on Reddit\]','',oldpagetext)
-        newpagetext = oldpagetext
-
+      if u"�" in oldpagetext:
+        print "found special on ", fileprefix
+        newpagetext = re.sub(u"�",'',oldpagetext)
         print newpagetext
         page.save(newpagetext, summary='munged')
